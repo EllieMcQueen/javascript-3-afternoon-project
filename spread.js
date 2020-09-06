ba112a905fab78def87bd6d599eede50cadf6b22
Ellie McQueen <elliemcqueen@gmail.com>
2020-09-06 17:14:04 -0600

    updated javascript 3

--- spread.js
+++ spread.js
@@ -19,6 +19,7 @@ let product = {
 
 // code here
 
+let productCopy = {...product}; 
 
 
 
@@ -28,7 +29,7 @@ let product = {
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+let colorsCopy = {...colors};
 
 
 
@@ -44,7 +45,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
+const result = addNums(...numbers);
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +55,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = arr1.concat(arr2);
